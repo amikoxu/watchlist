1fc6252636e40d1dca0429492545e10988465872
xu <amikoxu@163.com>
2019-11-27 20:05:48 +0800

    add index page

--- app.py
+++ app.py
@@ -1,7 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+name = "xu"
+movies = [
+    {'title':'my neirbor toroto','year':'1998'},
+    {'title':'dead poet society','year':'1989'},
+    {'title':'a perfect world','year':'1993'},
+    {'title':'mahjong','year':'1996'},
+    {'title':'the pork of music','year':'2012'}
+    ]
+
 @app.route('/')
-def hello():
-    return '<h1>welcome to watchlist~~ </h1>'
+def index():
+    
+    return render_template('index.html',name=name,movies=movies)
+
